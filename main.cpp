f4cf4010f711ec8fc107957e1ef46ced88cb6741
Michael Stanin <michael.stanin@gmail.com>
2019-04-05 11:28:17 +0300

    Example: NullPointerDereference

--- main.cpp
+++ main.cpp
@@ -2,38 +2,51 @@
 
 using namespace std;
 
-namespace DivisionByZero
+// namespace DivisionByZero
+// {
+//     //clang-tidy --checks=* main.cpp
+
+//     int division(int a, int b)
+//     {
+//         if ( b == 0 ) {
+//             throw "Division by zero condition!";
+//         }
+//         return a/b;
+//     }
+
+//     void test()
+//     {
+//         int x{10};
+//         int y{0};
+//         int z;
+
+//         try {
+//             z = division(x,y);
+//             cout << "The answer is " << z << endl;
+//         }
+//         catch (const char* msg) {
+//             cerr << "Caught an exception!" << endl;
+//             cerr << msg << endl;
+//         }
+//     }
+// }
+
+namespace NullPointerDereference
 {
-    //clang-tidy --checks=* main.cpp
-
-    int division(int a, int b)
-    {
-        if ( b == 0 ) {
-            throw "Division by zero condition!";
-        }
-        return a/b;
-    }
-
     void test()
     {
-        int x{10};
-        int y{0};
-        int z;
+        int* pi;
+        cout << *pi << endl;
 
-        try {
-            z = division(x,y);
-            cout << "The answer is " << z << endl;
-        }
-        catch (const char* msg) {
-            cerr << "Caught an exception!" << endl;
-            cerr << msg << endl;
-        }
+        int* otherPi = nullptr;
+        cout << *otherPi << endl;
     }
 }
 
 int main()
 {
     //std::cout << "Hello World";
-    DivisionByZero::test();
+    //DivisionByZero::test();
+
     return 0;
 }
